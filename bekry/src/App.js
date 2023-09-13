85703c03cb8c7c9828283e984575980571ea92fe
Wanton Raven <josephngalu96@gmail.com>
2023-09-13 12:04:41 +0000

    merging

--- bekry/src/App.js
+++ bekry/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          In ALX<code>  We rule</code>, we de best.
         </p>
         <a
           className="App-link"
