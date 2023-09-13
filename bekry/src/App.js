f64520e6947345e3291e6e32338b1a00ea534ccf
Wanton Raven <josephngalu96@gmail.com>
2023-09-13 18:44:45 +0000

    create routing

--- bekry/src/App.js
+++ bekry/src/App.js
@@ -1,20 +1,62 @@
+import React from 'react';
 import './App.css';
-import Login from './components/Login/Login';
-import ProductList from './components/Products/ProductList';
-import Cart from './components/Cart/Cart';
-import Occassion from './components/Occassions/Occasions';
-import Delivery from './components/SamedayDelivery/Samedaydelivery';
+import Login  from './components/Login/Login';
+import  ProductList  from './components/Products/ProductList';
+import  Cart  from './components/Cart/Cart';
+import  Occassion  from './components/Occassions/Occasions';
+import  Delivery from './components/SamedayDelivery/Samedaydelivery';
+import {
+  createBrowserRouter, 
+  Link,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+  
+} from 'react-router-dom';
+import ReactDOM from 'react-dom/client'
 
 function App() {
+  const route = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/occassion' element={<Occassion />}/>
+        <Route path='/product-list' element={<ProductList />}/>
+        <Route path='/delivery' element={<Delivery />}/>
+        <Route path='/cart' element={<Cart />}/>
+      </Route>
+      
+    )
+  )
   return (
     <div className="App">
-      <Login />
-      <ProductList />
-      <Cart />
-      <Occassion />
-      <Delivery />
+    <RouterProvider router={route} />
+  
     </div>
   );
+}
+
+const Root  =  () => {
+  return (
+    <>
+    <div>
+    <button>
+      <Link to='/login'>Login</Link>
+      </button>
+      <button>
+      <Link to='/occassion'>Occassion</Link>
+      </button>
+      <button>
+      <Link to='/product-list'>ProductList</Link>
+      </button>
+      <button>
+      <Link to='/delivery'>Delivery</Link>
+      </button><button>
+      <Link to='/cart'>Cart</Link>
+      </button>      
+    </div>
+    </>
+  )
 }
 
 export default App;
